7bbcd33e578354ff59c10de6a0c2e6f9c261e62e
Jordan Kniest <mail@jkniest.de>
2017-05-01 00:44:30 +0200

    Cleaned up some old files

---
+++ Assets/Scripts/Delta8x34/Objects/Furnace.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace LD38.Objects
+{
+    public class Furnace : Interactable
+    {
+        #region PROTECTED_VARS
+
+        [SerializeField] protected Transform Iron;
+
+        [SerializeField] protected Transform[] Fires;
+
+        protected bool IsOn;
+
+        #endregion
+
+        #region PUBLIC_METHODS
+
+        public bool On
+        {
+            get { return IsOn; }
+        }
+
+        #endregion
+
+        #region PROTECTED_METHODS
+
+        protected override void OnInteract()
+        {
+            if (Main.Get.Resources.Metal > 0)
+            {
+                Iron.gameObject.SetActive(true);
+            }
+
+            foreach (var fire in Fires)
+            {
+                fire.gameObject.SetActive(true);
+            }
+
+            IsOn = true;
+        }
+
+        #endregion
+    }
+}
